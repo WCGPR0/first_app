5a9c29662962d12a08c11f6572e853050e962284
Victor Wu <victor_moove@hotmail.com>
2013-10-12 05:02:06 +0000

    Fixed git repos

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ruby on Rails first App
+
+This is my first App .. #WCGPR0
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '1bf4dd05d9988e9e17ccc75f066e4e70031081741af763fa5dd20187db5579fffa56a32ad4323f49700cbfe6c073233dfeed5b425f1dc5ede5e5237dbc5742ec'
